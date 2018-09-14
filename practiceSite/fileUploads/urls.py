1a4a5dba3c05ef0f3b7aa293996efd869a7b6e16
RmarzooqUR <marzooq697231@gmail.com>
2018-09-15 01:47:30 +0530

    Seperate index and upload pages,add an alert on uploading the file

--- practiceSite/fileUploads/urls.py
+++ practiceSite/fileUploads/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'fileUploads'
 urlpatterns = [
-    path('',views.modelFormUpload, name = 'index'),
+    path('',views.IndexView.as_view(),name = 'index'),
+    path('<upload/>',views.modelFormUpload, name = 'upload'),
 ]
--- practiceSite/fileUploads/views.py
+++ practiceSite/fileUploads/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .forms import DocumentForm
 from django.http import HttpResponseRedirect
+from django.views import generic
+from django.contrib import messages
 # Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = 'fileUploads/index.html'
+
 def modelFormUpload(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
+            messages.info(request,'File was uploaded Successfully')
             return HttpResponseRedirect('')
+        else:
+            messages.info(request,'There was some issue in uploading the file please try again')
     else:
         form = DocumentForm()
-    return render(request,'fileUploads/upload.html',{'form':form})    
+    return render(request,'fileUploads/upload.html',{'form':form})
